f2c5d60e29568b936a0ef9998df45a56ac475cd0
Mogens Heller Grabe <mookid8000@gmail.com>
2016-08-31 11:36:27 +0200

    add files

---
+++ Rebus.Protobuf.Tests/ProtobufSerializerTests.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+using Rebus.Tests.Contracts.Serialization;
+
+namespace Rebus.Protobuf.Tests
+{
+    [TestFixture]
+    public class ProtobufSerializerTests : BasicSerializationTests<ProtobufSerializerFactory>
+    {
+    }
+}
